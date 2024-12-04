f0043faba74b470e9c7dda30e85ed7183ca6115a
GwynGO <131938005+Gavinwonder@users.noreply.github.com>
2024-12-04 17:18:42 +0800

    Add files via upload

---
+++ Different Python Data Types - an example.py
@@ -0,0 +1,17 @@
+# Author: Gavin
+# Time_Created: 12/4/24
+# Description:
+# an example for different Python data types such as
+# Number(Integer, Float, Complex),
+# String, Boolean, List, Tuple, Set and Dictionary.
+
+print(type(7)) # int
+print(type(11.10)) # float
+print(type(4-11j)) # complex
+print(type('Python is fun')) # str
+print(type(True)) # bool
+print(type(['The','Above','Is','True',7,11.10,4-11j,True])) # list
+print(type((1,[2,3],False,'456789'))) # tuple
+print(type({1,(2,3),True})) # set
+print(type({1:2,3:4,True:False})) # dict
+
